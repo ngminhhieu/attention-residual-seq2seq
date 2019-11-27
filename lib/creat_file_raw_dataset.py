8d94e91dd31d2f013876c865e8ba3ec4c6dbf554
Hieu Nguyen <minhhieun4@gmail.com>
2019-11-27 14:35:34 +0700

    fix config

--- lib/creat_file_raw_dataset.py
+++ lib/creat_file_raw_dataset.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    data = read_csv('../data/data.csv')
+    data = read_csv('../data/actual.csv')
     # shape (8759,1)
     data = data.groupby(['time'],as_index=False).agg({'load': 'sum'})
     load_data = data['load'].to_numpy()
     load_data = np.expand_dims(load_data, axis=1)
-    np.savez('../data/data.npz', data = load_data)
+    np.savez('../data/actual_data.npz', data = load_data)
 
